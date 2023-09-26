6e092d728353f3957196952e6f82fcf114b7c219
Alex Barna <flpbarna.alex@gmail.com>
2023-09-26 15:38:38 +0200

    fix routes

--- src/main/java/com/ge/test/ireport/rest/viewer/Authentication.java
+++ src/main/java/com/ge/test/ireport/rest/viewer/Authentication.java
@@ -1,9 +1,11 @@
 package com.ge.test.ireport.rest.viewer;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/viewer/authentication")
+@RestController()
+@RequestMapping("api/v1/viewer/authentication")
 public class Authentication {
 
     @GetMapping
