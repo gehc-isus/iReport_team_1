1e29f761c8ba9b5936076752080aef5c5a0aaecd
Alex Barna <flpbarna.alex@gmail.com>
2023-09-26 16:02:08 +0200

    repository,entity base, hosptal,doctor

--- src/main/java/com/ge/test/ireport/repository/HospitalRepository.java
+++ src/main/java/com/ge/test/ireport/repository/HospitalRepository.java
@@ -1,6 +1,5 @@
 package com.ge.test.ireport.repository;
 
-import com.ge.test.ireport.model.ListResponse;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
