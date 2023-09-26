8b94d3db687fe06223c4c6262562f433747d39cc
AstrA <gecsevar@gmail.com>
2023-09-26 15:38:19 +0200

    API  - auth mock

--- src/main/java/com/ge/test/ireport/rest/viewer/Authentication.java
+++ src/main/java/com/ge/test/ireport/rest/viewer/Authentication.java
@@ -1,14 +1,22 @@
 package com.ge.test.ireport.rest.viewer;
 
+import com.ge.test.ireport.model.AuthDataRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController("/api/v1/viewer/authentication")
 public class Authentication {
 
     @GetMapping
-    String getToken() {
+    String getToken(AuthDataRequest request) {
 
-        return "token";
+        if (Objects.equals(request.username, "Szabi") && Objects.equals(request.password, "A kiraj")) {
+
+            return "This is a valid TOKEN";
+        }
+
+        return "INVALID token";
     }
 }
