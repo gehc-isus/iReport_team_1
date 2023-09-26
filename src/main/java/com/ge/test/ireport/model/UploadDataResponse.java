01991c3adcbd76397b45137cf06fa624900343b1
Alex Barna <flpbarna.alex@gmail.com>
2023-09-26 15:54:22 +0200

    remove @entity

--- src/main/java/com/ge/test/ireport/model/UploadDataResponse.java
+++ src/main/java/com/ge/test/ireport/model/UploadDataResponse.java
@@ -3,9 +3,9 @@ package com.ge.test.ireport.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
+
 public class UploadDataResponse {
-    @Id
+
     private Long id;
 
     public void setId(Long id) {
